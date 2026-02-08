c492d4149c6218fcff9f6de68489f8c009f00db5
Boss17536 <Boss.927262@gmail.com>
2026-02-08 06:28:35 +0530

    Fix corrupted docs/page.tsx

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,1 +1,8 @@
-undefined
+export default function DocsPage() {
+  return (
+    <div className="min-h-screen bg-void text-white p-8">
+      <h1 className="text-4xl font-bold mb-4">Documentation</h1>
+      <p>Documentation coming soon...</p>
+    </div>
+  );
+}
